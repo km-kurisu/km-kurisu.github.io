1c8a672a6185c239bd8d82ba6ef64fb3a88e3c93
km-kurisu <kamleshkmistry33@gmail.com>
2024-08-17 19:37:07 +0530

    modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: "/react-portfolio",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
