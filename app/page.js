42efcf666ae58e94a3c03956a10c40d7342cb891
km-kurisu <kamleshkmistry33@gmail.com>
2025-05-20 10:56:49 +0530

    content change

--- app/page.js
+++ app/page.js
@@ -1,28 +1,23 @@
 'use client';
 import Head from "next/head";
 import React from "react";
-import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {
-  IoLogoLinkedin,
-  IoLogoGithub 
-} from 'react-icons/io5';
-import {AiFillInstagram} from 'react-icons/ai';
-import {BiLogoGmail} from 'react-icons/bi';
+import { MdOutlineDarkMode } from "react-icons/md";
+import { 
+  FiInstagram ,
+  FiLinkedin,
+  FiGithub,
+  FiMail
+} from "react-icons/fi";
+
 import Image from "next/image";
-import RuanMei from '../public/RuanMei.png';
 import design from '../public/design.png';
-import consulting from '../public/consulting.png';
 import code from '../public/code.png';
-import web1 from '../public/web1.png';
-import web2 from '../public/web2.png';
-import web3 from '../public/web3.png';
-import web4 from '../public/web4.png';
 import Design4 from '../public/Design4.png';
 import {useState} from 'react';
 
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <div className={darkMode? "dark" :""}>
       <Head>
@@ -36,32 +31,44 @@ export default function Home() {
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-slate-800">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-4xl text-slate-600 font-aktura dark:text-slate-300">Kamlesh Mistry</h1>
+            <h1 className="text-4xl text-slate-600 font-arrayr dark:text-slate-300">Kamlesh Mistry</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-xl  text-gray-600 dark:text-slate-300"/>
+                <MdOutlineDarkMode onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-3xl  text-gray-600 dark:text-slate-300"/>
               </li>
-              <li><a href="#" className="bg-gradient-to-r from-slate-300 to-teal-200 text-white px-4 py-2 rounded-3xl ml-8 dark:text-gray-700">Resume</a></li>
+              <li>
+                <a
+                  href="/KamleshMistry.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-gradient-to-r from-slate-300 to-teal-200 text-white px-4 py-2 rounded-sm ml-8 dark:text-gray-700"
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
           </nav>
           <div className="text-center p-10 ">
-            <h2 className="text-5xl py-2 text-slate-600 font-medium md:text-6xl dark:text-gray-50 ">
+            <h2 className="text-5xl py-2 font-arrayb text-slate-600 font-medium md:text-6xl dark:text-gray-50 ">
               Kamlesh Mistry
               </h2>
-            <h3 className="text-2xl py-2 text-teal-600 md:text-3xl dark:text-teal-300">
+            <h3 className="text-2xl font-nippo py-2 text-teal-600 md:text-3xl dark:text-teal-300">
               Developer, Desginer and Artist.
               </h3>
             <p className="text-md py-5 leading-8 text-teal-800 md:text-xl max-w-xl mx-auto dark:text-slate-300">
-            I possess a unique blend of skills that allow me to bridge the gap between creativity and technology. 
-            I can seamlessly transition from coding intricate algorithms to crafting visually stunning designs, all while infusing my work with a touch of artistic flair. 
-            My ability to combine technical expertise with creative vision makes me a versatile asset in the digital world.
+            Based in Virar, Maharashtra, India.</p>
+            <p className="text-md py-5 leading-8 text-teal-800 md:text-xl max-w-xl mx-auto dark:text-slate-300">
+            Completed my SSC in 2019, HSC in 2021 and Diploma in Computer Engineering in 2024.
+            </p>
+            <p className="text-md py-5 leading-8 text-teal-800 md:text-xl max-w-xl mx-auto dark:text-slate-300">
+            Curretly pursuing my Bachelors in technology in Computer Engineering  St. John College of Engineering and Management based in Palghar, Maharashtra, India.
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-slate-300"> 
-           <a href="https://www.instagram.com/kamleshm_02/" target="_blank"> <AiFillInstagram /></a>
-            <a href="https://www.linkedin.com/in/kamlesh-mistry-957895260" target="_blank"> <IoLogoLinkedin /></a>
-           <a href="https://github.com/km-kurisu" target="_blank"> <IoLogoGithub /></a>
-           <a href="mailto:kamleshkmistry33@gmail.com" target="_blank"> <BiLogoGmail /></a>
+           <a href="https://www.instagram.com/kamleshm_02/" target="_blank"> <FiInstagram /></a>
+            <a href="https://www.linkedin.com/in/kamlesh-mistry-957895260" target="_blank"> <FiLinkedin /></a>
+           <a href="https://github.com/km-kurisu" target="_blank"> <FiGithub /></a>
+           <a href="mailto:kamleshkmistry33@gmail.com" target="_blank"> <FiMail /></a>
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-slate-500 rounded-full w-90 h-90 mt-20 mb-20 md:h-96 md:w-96 md:mb-40 ">
             <Image src={Design4}  layout="Fill" objectFit="cover"/>
@@ -69,80 +76,77 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className="text-4xl font-aktura py-1 text-teal-600 dark:text-teal-300">Services i offer</h3>
+            <h3 className="text-4xl font-arrayr py-1 dark:text-teal-300">About Me</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-300" >
-              As a Developer, Designer, and Artist, I offer a comprehensive range of services tailored to your digital needs. My expertise includes:
+              Creative and Detailed-oriented developer with a passion for creating
+              engaging and user-friendly digital experiences. I have a strong foundation in both front-end and back-end development, as well as a variety of different skills
+              providing versititlity and adaptability in the fast-paced world of technology.
+            </p>
+            <h3 className="text-4xl font-arrayw py-1 dark:text-teal-300">Skills</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-300 ">
+              <span className="font-nippo text-xl text-teal-600 dark:text-emerald-400">Web Development:</span> Building custom websites with a focus on user experience, functionality, and aesthetics.
               </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-300 ">
-              <span className="font-aktura text-xl text-teal-600 dark:text-emerald-400">Web Development:</span> Building custom websites with a focus on user experience, functionality, and aesthetics.
-              </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-300 ">
-            <span className="font-aktura text-xl text-teal-600 dark:text-emerald-400">Digital Art: </span>Producing original artwork, illustrations, and graphic design elements.
+            <span className="font-nippo text-xl text-teal-600 dark:text-emerald-400">Digital Art: </span>Producing original artwork, illustrations, and graphic design elements.
                </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-300 ">  
-            <span className="font-aktura text-xl text-teal-600 dark:text-emerald-400">Full-Stack Development:</span> Handling all aspects of software development, from front-end to back-end.
+            <span className="font-nippo text-xl text-teal-600 dark:text-emerald-400">Full-Stack Development:</span> Handling all aspects of software development, from front-end to back-end.
               </p>
               <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-300 ">  
-              <span className="font-aktura text-xl text-teal-600 dark:text-emerald-400">Application Development:</span> Building custom applications with a focus on user experience, functionality, and aesthetics.
+              <span className="font-nippo text-xl text-teal-600 dark:text-emerald-400">Application Development:</span> Building custom applications with a focus on user experience, functionality, and aesthetics.
               </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-left shadow-xl p-10 rounded-xl my-10 flex-1 text-teal-600 dark:bg-slate-700 dark:shadow-2xl">
+            <div className="text-left shadow-xl p-10 rounded-xl my-10 flex-1 dark:bg-slate-700 dark:shadow-2xl">
               <Image src={design} width={100} height={100} />
-              <h3 className="text-2xl font-aktura font-medium pt-8 pb-2 dark:text-teal-300 ">
-                Beautiful Designs
+              <h3 className="text-2xl font-arrayb font-medium pt-8 pb-2 text-teal-600 dark:text-teal-300 ">
+                Achievements
               </h3>
-              <p className="py-2 text-teal-600 dark:text-slate-300">
-                Creating elegant designs suited for your needs following core
-                design theory.
+              <p className="py-2 font-nippo text-teal-600 dark:text-slate-300">
+                A Few Achievements and Certifications I have earned over the years.
               </p>
-              <h4 className="py-4 text-teal-600 dark:text-emerald-400">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Photoshop</p>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Illustrator</p>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Figma</p>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Canva</p>
+              <h4 className="py-4 text-teal-600 dark:text-emerald-400">Participation in TechSurf Hackathon in 2023 </h4>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Participated in HackON with Amazon S-3</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Pursuing CS50X Online Course by Harvard</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Pursuing CS50X Game Development Online Course by Harvard</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Completed DotNET and SQL training course led by Kantascrypt</p>
+            </div>
+            <div className="text-left shadow-xl p-10 rounded-xl my-10 flex-1 dark:bg-slate-700 dark:shadow-2xl">
+              <Image src={design} width={100} height={100} />
+              <h3 className="text-2xl font-arrayb font-medium pt-8 pb-2 text-teal-600 dark:text-teal-300 ">
+                Future Goals
+              </h3>
+              <p className="py-2 font-nippo text-teal-600 dark:text-slate-300">
+                Some Goals that i want to achieve
+              </p>
+              <h4 className="py-4 text-teal-600 dark:text-emerald-400">Complete my B.Tech with specializtion in AI</h4>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Create at least 4 games before the end of 3rd Year</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Land an InternShip</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">work towards creating my own product based startup</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Atleast Visit japan Once</p>
             </div>
             <div className="text-left shadow-xl p-10 rounded-xl my-10 flex-1 dark:bg-slate-700 dark:shadow-2xl">
               <Image src={code} width={100} height={100} />
-              <h3 className="text-2xl font-aktura font-medium pt-8 pb-2 dark:text-teal-300 ">
-                Code your dream project
+              <h3 className="text-2xl font-arrayb font-medium pt-8 pb-2 text-teal-600 dark:text-teal-300 ">
+                Personal Side
               </h3>
-              <p className="py-2 text-teal-600 dark:text-slate-300">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
+              <p className="py-2 font-nippo text-teal-600 dark:text-slate-300">
+                A insight into my personal side
               </p>
-              <h4 className="py-4 text-teal-600 dark:text-emerald-400">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Android Studio</p>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Visual Studio Code</p>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Visual Studio</p>
-              <p className="text-gray-800 py-1 dark:text-emerald-400">Unity 3D</p>
+              <h4 className="py-4 text-teal-400  dark:text-emerald-400">A Reader, Writer, Observer</h4>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Wrote and Proofread many webnovels for various scanalations</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">A hobbyist Digital Illustrator and learning 3D art</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">Love to observe and find the hidden meaning behind the nature's motives</p>
+              <p className="text-gray-800 py-1 dark:text-emerald-400">A avid watcher of skies</p>
             </div>
           </div>
         </section>
         <section>
-          <div>
-            <h3 className="font-aktura text-4xl py-1 dark:text-teal-300">Portfolio</h3>
-            <p className="text-md py-5 leading-8 text-teal-800 dark:text-slate-300">
-              My portfolio showcases a diverse range of projects that highlight my technical skills, 
-              creative vision, and problem-solving abilities. Each piece reflects my commitment to crafting innovative and visually appealing digital solutions. 
-              From responsive websites to engaging mobile apps and captivating artwork, 
-              my portfolio demonstrates my ability to seamlessly blend creativity and technology.
-            </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1">
-              <Image src={web1} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive" />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image src={web2} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive" />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image src={web3} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive" />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image src={web4} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive" />
-            </div>
-          </div>
+         <div>
+          <footer className="text-gray-800 text-center py-1 font-nippo dark:text-emerald-400"> 
+            <p>&copy; 2025 Kamlesh Mistry. All rights reserved.</p>
+            </footer>
+          </div>        
         </section>
       </main>
     </div>
